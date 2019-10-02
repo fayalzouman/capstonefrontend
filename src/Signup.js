f61327c7c03c7c48a1f3e9427ef5390b6ea88fce
Fay <fayalzouman@gmail.com>
2019-10-02 19:24:24 +0300

    authentication

---
+++ src/Signup.js
@@ -0,0 +1,73 @@
+import React, { Component } from "react";
+
+class Signup extends Component {
+  state = {
+    username: "",
+    email: "",
+    password: ""
+  };
+
+  handleChange = event =>
+    this.setState({ [event.target.name]: event.target.value });
+
+  handleSubmit = event => {
+    event.preventDefault();
+    alert("SIGNUP: CONNECT ME!");
+  };
+
+  render() {
+    const { username, email, password } = this.state;
+    return (
+      <div className="col-6 mx-auto">
+        <div className="card my-5">
+          <div className="card-body">
+            <form onSubmit={this.handleSubmit}>
+              <div className="form-group">
+                <label htmlFor="username">Username</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="username"
+                  value={username}
+                  name="username"
+                  placeholder="Username"
+                  onChange={this.handleChange}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="email">Email</label>
+                <input
+                  type="email"
+                  className="form-control"
+                  id="email"
+                  value={email}
+                  name="email"
+                  placeholder="Email"
+                  onChange={this.handleChange}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="password">Password</label>
+                <input
+                  type="password"
+                  className="form-control"
+                  id="password"
+                  value={password}
+                  name="password"
+                  placeholder="Password"
+                  onChange={this.handleChange}
+                />
+              </div>
+
+              <button type="submit" className="btn btn-primary">
+                Signup
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Signup;
